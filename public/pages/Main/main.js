b0059af23487a698609a7ae2f0451a82282e7d94
kirill555101 <kir20-01@ya.ru>
2022-03-07 00:50:16 +0300

    Some fixes in code

--- public/pages/Main/main.js
+++ public/pages/Main/main.js
@@ -1,9 +1,9 @@
 import components from '../../components/import.js';
 import UIKIT from '../../ui-kit/import.js';
 
-const imgPath = 'https://citrusfoto.ru/sites/default/files/styles/work_samples/public/1000-vasiliy_malykhin-4dfd2881e419f552c3fa1208ff4e8535.jpg?itok=ZE_5-a43';
-
 const mainPage = (root) => {
+    const imgPath = 'https://citrusfoto.ru/sites/default/files/styles/work_samples/public/1000-vasiliy_malykhin-4dfd2881e419f552c3fa1208ff4e8535.jpg?itok=ZE_5-a43';
+
     const restaurants = [
         { imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8 },
         { imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8 },
