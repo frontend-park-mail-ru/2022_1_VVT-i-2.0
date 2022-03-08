af3fc5a1f1d25cb0b01d765f9b7d175fb195713a
kirill555101 <kir20-01@ya.ru>
2022-03-09 01:40:30 +0300

    Removed restarants from main page

--- public/pages/Main/main.js
+++ public/pages/Main/main.js
@@ -4,23 +4,6 @@ import * as events from '../../events/events.js';
 import UIKIT from '../../ui-kit/import.js';
 
 const mainPage = (app) => {
-    const imgPath = 'https://citrusfoto.ru/sites/default/files/styles/work_samples/public/1000-vasiliy_malykhin-4dfd2881e419f552c3fa1208ff4e8535.jpg?itok=ZE_5-a43';
-
-    const restaurants = [
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-        {imgPath, restName: 'Шоколадница', timeToDeliver: '20-35 мин', price: '550₽', rating: 4.8},
-    ];
-
     api
         .getRestaurants()
         .then((res) => res.json())
@@ -36,13 +19,6 @@ const mainPage = (app) => {
 
             events.addListeners(app);
         });
-
-    app.root.innerHTML = components.header(false);
-
-    const main = document.createElement('main');
-    main.innerHTML = UIKIT.mainLink('Рестораны') + components.restIcons(restaurants);
-
-    app.root.appendChild(main);
 };
 
 export default mainPage;
