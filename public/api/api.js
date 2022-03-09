41b72d8e5b89c56b6fc5d9b513417018feeb4215
kirill555101 <kir20-01@ya.ru>
2022-03-09 08:24:11 +0300

    Fixed problem with post request

--- public/api/api.js
+++ public/api/api.js
@@ -11,6 +11,7 @@ const DEFAULT_OPTIONS = {
 const request = (url, options = DEFAULT_OPTIONS) => {
     options.credentials = 'include';
     if (options.body) {
+        headers['Content-Type'] = 'application/json';
         options.body = JSON.stringify(options.body);
     }
     return fetch(BASE_URI + '/api/v1' + url, options);
