d39363ccc5edec17df48542cfb3f8a252fcf3253
Andrey123815 <ivredomon@gmail.com>
2022-04-08 22:43:22 +0300

    switcher refactoring

--- public/ui-kit/Switcher/switcher.js
+++ public/ui-kit/Switcher/switcher.js
@@ -1,7 +1,7 @@
 const switcher = () => {
     return `
-        <div id="mail-notification">
-            <div id="moving-ball"></div>
+        <div id="mail-notification" class="mail-notification__switcher-block">
+            <div id="moving-ball" class="switcher-block__moving-ball"></div>
         </div>
     `;
 
