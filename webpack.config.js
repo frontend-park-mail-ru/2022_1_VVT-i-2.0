08bbf531e55db7e54ba817db9b83a664622dcb24
Andrey123815 <ivredomon@gmail.com>
2022-04-11 02:59:26 +0300

    add single css file for all project, start working on postCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: "production",
     entry: "./public/index.js",
-    devtool: "source-map",
+    devtool: "eval-source-map",
     output: {
-        path: path.resolve(__dirname, 'public/bundle'),
+        path: path.resolve(__dirname, './public/bundle'),
         filename: 'bundle.js',
+        publicPath: 'public/bundle',
     },
     module: {
         rules: [
@@ -21,58 +23,39 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.pcss$/,
+                test: /\.scss$/,
                 use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
                     {
-                        loader: 'style-loader',
-                        options: {
-                            singleton: true,
-                        },
-                    },
-                    'postcss-loader',
-                ],
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: { sourceMap: true, config: { path: 'postcss.config.js'} }
+                    }, {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
             },
             {
-                test: /\.s[ac]ss$/i,
+                test: /\.css$/,
                 use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.svg/,
-                use: {
-                    loader: 'svg-url-loader',
-                    options: {},
-                },
-            },
-            // {
-            //     test: /\.(img|jpeg|jpg|png)$/,
-            //     loader: 'file-loader?name=img/[name].[ext]',
-            // },
-            // {
-            //     test: /\.(ico)$/,
-            //     loader: 'file-loader?name=favicon.ico',
-            // },
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
         ]
     },
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         template: __dirname + "/src/public/index.html",
-    //         inject: 'body'
-    //     }),
-    // ],
-    devServer: {
-        historyApiFallback: true,
-        port: 3000,
-        open: true,
-    }
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css',
+        })
+    ],
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
 };
