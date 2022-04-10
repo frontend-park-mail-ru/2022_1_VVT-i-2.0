ad838839b8d19b88b85edebd8ff0657e182ebe8c
Andrey123815 <ivredomon@gmail.com>
2022-04-10 13:26:07 +0300

    BAM header

--- public/pages/Main/main.js
+++ public/pages/Main/main.js
@@ -2,10 +2,7 @@ import components from '../../components/import.js';
 import * as events from '../../events/events.js';
 import UIKIT from '../../ui-kit/import.js';
 
-/**
- * @function Рендерит страницу по входящему объекту приложения.
- * @param {Object} app - Объект приложения.
- */
+
 const mainPage = (app, store) => {
     // api
     //     .getRestaurants()
@@ -13,11 +10,7 @@ const mainPage = (app, store) => {
     //     .then((res) => {
     // events.removeListeners(app);
 
-<<<<<<< HEAD
     app.root.innerHTML = components.header(true, 'TRASH/avatar.jpg');
-=======
-    // app.root.innerHTML = components.header(true);
->>>>>>> 386289ecc3c6a287152ba01bdb2b0a6f89a2e5d8
 
     // const main = document.createElement('main');
     // main.innerHTML = UIKIT.mainLink('Рестораны') + components.restIcons({});
