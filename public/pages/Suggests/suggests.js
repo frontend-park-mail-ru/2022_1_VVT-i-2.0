d39eb19e3269eca07c5d0effe7e183f84ddfaff9
kirill555101 <kir20-01@ya.ru>
2022-04-13 12:57:18 +0300

    Fixes

--- public/pages/Suggests/suggests.js
+++ public/pages/Suggests/suggests.js
@@ -1,10 +1,10 @@
 import components from '../../components/import.js';
 
 const suggestsPage = (app, store) => {
-  const suggests = [
-    { address: 'FIRST', end: false }, { address: 'SECOND', end: true }
-  ];
-  // const suggests = store.getters.suggests();
+  // const suggests = [
+  //   { address: 'FIRST', end: false }, { address: 'SECOND', end: true }
+  // ];
+  const suggests = store.getters.suggests();
   app.modal.innerHTML = components.suggestsForm({ suggests });
 };
 
