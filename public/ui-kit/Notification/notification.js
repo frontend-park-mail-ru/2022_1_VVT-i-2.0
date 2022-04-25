0b12b96cbff9e15b7440e8ae9b4aa0a924ce087a
kirill555101 <kir20-01@ya.ru>
2022-04-25 23:15:24 +0300

    Changes in ui-notification + added icons

---
+++ public/ui-kit/Notification/notification.js
@@ -0,0 +1,9 @@
+const notification = () => {
+  const template = `
+    <div class="notification">
+      <a class="links-block__main-link">{{title}}</a>
+    </div>
+  `;
+};
+
+export default notification;
