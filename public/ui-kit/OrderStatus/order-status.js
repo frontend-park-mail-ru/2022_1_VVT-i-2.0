5451a097f6dc46432e9515ae6dc4345b60eb0f9b
Andrey123815 <ivredomon@gmail.com>
2022-05-01 09:51:13 +0300

    add orderStatus

---
+++ public/ui-kit/OrderStatus/order-status.js
@@ -0,0 +1,55 @@
+const orderStatus = (props, status) => {
+    let statusClass = '';
+
+    switch (status) {
+        case 'В обработке':
+            statusClass = 'status__img_in-processing';
+            break;
+        case 'Готовим':
+            statusClass = 'status__img_cooking';
+            break;
+        case 'В пути':
+            statusClass = 'status__img_in-way';
+            break;
+        default:
+            statusClass = 'status__img_received';
+            break;
+    }
+
+    const template = `
+        <div class="order-info">
+            <div class="order-info__order-number">
+                #2342
+            </div>
+            <div class="order-info__rest-name">
+                Макдональдс
+            </div>
+            <div class="order-info__order-date">
+                28.01.2022
+            </div>
+            <div class="order-info__order-price">
+                298 ₽
+            </div>
+            <div class="order-info__status">
+                <div class="{{statusClass}}"></div>
+                <div class="status__text">
+                    {{status}}
+                </div>
+            </div>
+            <div>
+                <img src="arrow-down.svg" alt="">
+            </div>
+        </div>
+    `;
+
+    return Mustache.render(template, {
+        orderNumber: props.orderNumber,
+        restName: props.restName,
+        date: props.date,
+        price: props.price,
+        statusClass,
+        status
+    });
+};
+
+export default orderStatus;
