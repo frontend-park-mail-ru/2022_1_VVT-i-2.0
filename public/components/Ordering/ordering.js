9e3681715a95b7ac5e70022e4124afc945e011c5
kirill555101 <kir20-01@ya.ru>
2022-05-04 01:58:14 +0300

    Added ordering mobile page

--- public/components/Ordering/ordering.js
+++ public/components/Ordering/ordering.js
@@ -2,16 +2,22 @@ import UIKIT from "../../ui-kit/import.js";
 import FORMS_CONFIGURATION from "../../configurations/forms.js";
 import paymentChoice from "../PaymentChoice/payment-choice.js";
 import { NumberPhoneFormat } from "../../events/entity/phone/phone-src";
+import ELEMS_CONFIGURATION from "../../configurations/elems.js";
 
 const ordering = (props) => {
   const inputConfigurations = FORMS_CONFIGURATION.inputs.ordering;
 
   const isEmpty = props.total === 0;
 
+  const isMobile = window.screen.width < 920;
+
   const template = `
-    <div>
+    {{^isMobile}}
+      <div>
         <img class="ordering-page__week-dish" src="/graphics/images/order_week_dish.png" alt="">
-    </div>
+      </div>
+    {{/isMobile}}
+
     <div class="ordering-page">
       {{&title}}
 
@@ -20,12 +26,12 @@ const ordering = (props) => {
           <div class="ordering-block__delivery-header">Доставка</div>
 
           {{#inputConfigurations.largeInputs}}
-                {{&input}}
+            {{&input}}
           {{/inputConfigurations.largeInputs}}
 
           <div class="ordering-block__exact-address">
             {{#inputConfigurations.smallInputs}}
-                  {{&input}}
+              {{&input}}
             {{/inputConfigurations.smallInputs}}
           </div>
 
@@ -37,14 +43,16 @@ const ordering = (props) => {
             placeholder="Напишите как Вас найти или пожелания для блюд..."
           ></div>
 
-          <div class="ordering-block__order-header">Оплата</div>
-          {{&paymentChoices}}
-          {{&buttonPay}}
+          {{^isMobile}}
+            <div class="ordering-block__order-header">Оплата</div>
+            {{&paymentChoices}}
+            {{&buttonPay}}
+          {{/isMobile}}
         </div>
         <div class="ordering-page__shopping-cart">
           {{^isEmpty}}
-          <div class="shopping-cart__rest-main-info shopping-cart__order-point">
-              <div class="shopping-cart__preview-rest">Ваш заказ в ресторане: {{restName}}</div>
+          <div class="shopping-cart__info-about-rest shopping-cart__order-point">
+            <div class="shopping-cart__preview-rest">Ваш заказ в ресторане: {{restName}}</div>
           </div>
 
           <div class="shopping-cart_order-points">
@@ -73,10 +81,17 @@ const ordering = (props) => {
           {{/isEmpty}}
         </div>
       </div>
+
+      {{#isMobile}}
+        <div class="ordering-block__order-header">Оплата</div>
+        {{&paymentChoices}}
+        {{&buttonPay}}
+      {{/isMobile}}
     </div>
   `;
 
   return Mustache.render(template, {
+    isMobile,
     restName: props.restName,
     inputConfigurations: inputConfigurations,
     orderPoints: props.orderPoints,
@@ -96,10 +111,16 @@ const ordering = (props) => {
         value = localStorage.getItem("address");
         readonly = true;
       }
+
+      let width = this.width;
+      if (isMobile && width === ELEMS_CONFIGURATION.inputs.VERY_LARGE) {
+        width = 343;
+      }
+
       return UIKIT.input(
         this.title,
         this.type,
-        this.width,
+        width,
         this.placeholder,
         this.id,
         "",
