b0c5d58b4ffbd87536b19f6936f45535c937f02c
Andrey123815 <ivredomon@gmail.com>
2022-05-06 10:42:02 +0300

    remove searchBlock to ui-kit

--- public/components/Header/header.js
+++ public/components/Header/header.js
@@ -28,19 +28,7 @@ const header = (isOrderingPage = false) => {
             {{^isOrderingPage}}
 
                 {{#isSearchActivated}}
-                <nav id="searchActivatedAria" class="searchActivatedAria">
-                    <div class="search-block">
-                        <div class="search-block__block-img">
-                            <img class="block-img__image" src="/graphics/icons/search_grey.svg" alt="">
-                        </div>
-                        <div class="search-block__input-block">
-                            <input id="searchInput" class="input-block__input-without-border" placeholder="Ресторан или блюдо" type="text">
-                        </div>
-                        <div class="search-block__block-img">
-                            <img id="closeImg" class="block-img__image" src="/graphics/icons/close.svg" alt="">
-                        </div>
-                    </div>
-                </nav>
+                  {{&searchBlockAriaActivated}}
                 {{/isSearchActivated}}
 
                 {{^isSearchActivated}}
@@ -115,6 +103,9 @@ const header = (isOrderingPage = false) => {
     backToMenu() {
       return UIKIT.backButton("Обратно в меню", "dishes");
     },
+    searchBlockAriaActivated() {
+      return UIKIT.searchBlock();
+    }
   });
 };
 
---
+++ public/ui-kit/SearchBlock/search-block.js
@@ -0,0 +1,21 @@
+const searchBlock = () => {
+    const template = `
+        <nav id="searchActivatedAria" class="searchActivatedAria">
+            <div class="search-block">
+                <div class="search-block__block-img">
+                    <img class="block-img__image" src="/graphics/icons/search_grey.svg" alt="">
+                </div>
+                <div class="search-block__input-block">
+                    <input id="searchInput" class="input-block__input-without-border" placeholder="Ресторан или блюдо" type="text">
+                </div>
+                <div class="search-block__block-img">
+                    <img id="closeImg" class="block-img__image" src="/graphics/icons/close.svg" alt="">
+                </div>
+            </div>
+        </nav>
+    `;
+
+    return Mustache.render(template, {});
+}
+
+export default searchBlock;
