fd1f4dacda595238db24174257ce1cdd3b2a533d
kirill555101 <kir20-01@ya.ru>
2022-05-16 00:03:00 +0300

    Header + suggests fix

--- public/components/SuggestsForm/suggests-form.js
+++ public/components/SuggestsForm/suggests-form.js
@@ -1,41 +1,35 @@
 const suggestsForm = (props) => {
-  const isMobile = window.screen.width < 920;
-
   const template = `
     <form id="suggest-form" class="suggest-form">
       {{#suggests}}
         <div class="suggest-form__suggest-row">
-          {{^isMobile}}<div></div>{{/isMobile}}
+          <div></div>
           <div class="suggest-row__suggest-address suggestsRow" end="{{end}}">
             {{address}}
           </div>
-          {{^isMobile}}
-            {{#auth}}
-              <div></div>
-            {{/auth}}
-          {{/isMobile}}
-          {{^isMobile}}<div></div>{{/isMobile}}
-          {{^isMobile}}<div></div>{{/isMobile}}
+          {{#auth}}
+            <div></div>
+          {{/auth}}
+          <div></div>
+          <div></div>
         </div>
       {{/suggests}}
       {{^suggests}}
         <div class="suggest-form__suggest-row">
-          {{^isMobile}}<div></div>{{/isMobile}}
+          <div></div>
           <div class="suggest-row__suggest-address suggestsRow" default="true">
             Мы подберем для вас список доступных ресторанов
           </div>
-          {{^isMobile}}
-            {{#auth}}
-              <div></div>
-            {{/auth}}
-          {{/isMobile}}
-          {{^isMobile}}<div></div>{{/isMobile}}
-          {{^isMobile}}<div></div>{{/isMobile}}
+          {{#auth}}
+            <div></div>
+          {{/auth}}
+          <div></div>
+          <div></div>
         </div>
       {{/suggests}}
     </form>
   `;
-  return Mustache.render(template, { isMobile, auth: props.auth, suggests: props.suggests });
+  return Mustache.render(template, { auth: props.auth, suggests: props.suggests });
 };
 
 export default suggestsForm;
