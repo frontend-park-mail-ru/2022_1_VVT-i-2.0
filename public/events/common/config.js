2d65c5c34018e973c2557fad171fd59b5ac14e5b
Andrey123815 <ivredomon@gmail.com>
2022-05-17 02:54:08 +0300

    name length in validation change

--- public/events/common/config.js
+++ public/events/common/config.js
@@ -3,7 +3,7 @@ export const Regex = {
   numericSymbol: /[0-9]/,
   confirmCode: /[0-9]{4}/,
   phoneNumber: /^[+]7[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}$/,
-  name: /^[A-ZА-Я]{1}[a-zа-я]{2,25}$/,
+  name: /^[A-ZА-Я]{1}[a-zа-я]{1,25}$/,
   email:
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 };
@@ -21,7 +21,7 @@ export const EntityLengthLimit = {
 
 export const ErrorMsg = {
   errorPhoneNumber: "Формат телефона: +7(988)888-88-88",
-  errorName: "Длина не менее 3 букв",
+  errorName: "Длина не менее 2 букв",
   errorEmail: "Формат email: fobrinto@gmail.com",
   errorConfirmCode: "Формат кода: 6666",
   errorEmptyInput: "Заполните это поле",
