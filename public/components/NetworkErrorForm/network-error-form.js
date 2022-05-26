1389b3a5cdcff1d10cbf84817094b64612330baf
kirill555101 <kir20-01@ya.ru>
2022-05-26 10:25:12 +0300

    Fixes in network-error-form

--- public/components/NetworkErrorForm/network-error-form.js
+++ public/components/NetworkErrorForm/network-error-form.js
@@ -1,6 +1,4 @@
 const networkErrorForm = ({ header, text }) => {
-  const isMobile = window.screen.width < 1460;
-
   const template = `
             <section class="error-page">
                 <div class="error-page__view">
@@ -16,17 +14,15 @@ const networkErrorForm = ({ header, text }) => {
                         </div>
                     </div>
 
-                    {{^isMobile}}
-                        <div class="view__graphic-content">
-                            <div class="graphic-content__img">
-                                <img src="/graphics/images/godzilla.png" alt="">
-                            </div>
+                    <div class="view__graphic-content">
+                        <div class="graphic-content__img">
+                            <img src="/graphics/images/godzilla.png" alt="">
                         </div>
-                    {{/isMobile}}
+                    </div>
                 </div>
             </section>
         `;
-  return Mustache.render(template, { isMobile, header, text });
+  return Mustache.render(template, { header, text });
 };
 
 export default networkErrorForm;
