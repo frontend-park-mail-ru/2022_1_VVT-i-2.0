8bd3221dc6bf9e6799d0c4dd4391568de23776c4
kirill555101 <kir20-01@ya.ru>
2022-06-02 21:05:33 +0300

    Updated ui-notification + changed active category color

--- public/ui-kit/Notification/notification.js
+++ public/ui-kit/Notification/notification.js
@@ -1,12 +1,6 @@
 const notification = (message, error) => {
   const template = `
-    <div class="notification">
-      {{#error}}
-        <img class="notification__img" src="/graphics/icons/notification-error.svg">
-      {{/error}}
-      {{^error}}
-        <img class="notification__img" src="/graphics/icons/notification-success.svg">
-      {{/error}}
+    <div class="notification {{#error}} notification_error {{/error}} {{^error}} notification_success {{/error}}">
       <div class="notification__message">{{message}}</div>
       <img class="notification__close-img" src="/graphics/icons/close.svg">
     </div>
